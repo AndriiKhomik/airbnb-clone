75c03f8d2230cde7a59eb52b04b4004283028782
Andrii <andrii.khomik@gmail.com>
2023-07-06 13:59:52 +0300

    add listing grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,10 @@ export default async function RootLayout({
           <RegisterModal />
           <LoginModal />
           <RentModal />
+          {/* @ts-ignore-next-line */}
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );

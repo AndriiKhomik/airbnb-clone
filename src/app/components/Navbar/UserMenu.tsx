d9dbb78bf87103789596db6063d70454205d976b
Andrii <andrii.khomik@gmail.com>
2023-07-10 14:34:16 +0300

    implament trips page

--- src/app/components/Navbar/UserMenu.tsx
+++ src/app/components/Navbar/UserMenu.tsx
@@ -9,12 +9,14 @@ import useLoginModal from "@/app/hooks/useLoginModal";
 import useRentModal from "@/app/hooks/useRentModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
+import { useRouter } from "next/navigation";
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
 }
 
 const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
@@ -57,7 +59,13 @@ const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
           <div className="flex flex-col cursor-pointer">
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label="My trips" />
+                <MenuItem
+                  onClick={() => {
+                    router.push("/trips");
+                    setIsOpen(false);
+                  }}
+                  label="My trips"
+                />
                 <MenuItem onClick={() => {}} label="My favorites" />
                 <MenuItem onClick={() => {}} label="My reservations" />
                 <MenuItem onClick={() => {}} label="My properties" />
---
+++ src/app/trips/TripsClient.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React, { FC, useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
+import { SafeReservation, SafeUser } from "../types";
+
+interface TripsClientProps {
+  reservations: SafeReservation[];
+  currentUser?: SafeUser | null;
+}
+
+const TripsClient: FC<TripsClientProps> = ({ reservations, currentUser }) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCalcel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success("Reservation canceled");
+          router.refresh();
+        })
+        .catch((error) => toast.error(error?.response?.data?.error))
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading
+        title="Trips"
+        subtitle="Where you've been and where you're going"
+      />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {reservations.map((reservation) => (
+          <ListingCard
+            data={reservation.listing}
+            key={reservation.id}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCalcel}
+            disabled={deletingId === reservation.id}
+            actionlabel="Cancel reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default TripsClient;
